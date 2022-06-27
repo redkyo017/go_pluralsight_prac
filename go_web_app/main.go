bbbc3dc2d11bdf3706e45be6e3b70d6fc3a55cc9
hung.han <hung.han@setel.com>
2022-06-27 23:46:51 +0700

    template

--- go_web_app/main.go
+++ go_web_app/main.go
@@ -1,10 +1,9 @@
 package main
 
 import (
-	"io"
+	"html/template"
 	"log"
 	"net/http"
-	"os"
 )
 
 // type myHandler struct {
@@ -15,33 +14,67 @@ import (
 // 	w.Write([]byte(fmt.Sprintf("%v world", myHl.greating)))
 // }
 
+// func main() {
+// 	// log.Println("con co be be")
+// 	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 	// 	w.Write([]byte("ha ha ha ha"))
+// 	// })
+// 	// http.Handle("/", &myHandler{greating: "bonjour"})
+// 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+// 		// http.ServeFile(w, r, "public"+r.URL.Path)
+// 		f, err := os.Open("public" + r.URL.Path)
+// 		if err != nil {
+// 			w.WriteHeader(http.StatusInternalServerError)
+// 			log.Println(err)
+// 		}
+// 		defer f.Close()
+// 		// var contentType string
+// 		// switch {
+// 		// case strings.HasSuffix(r.URL.Path, "html"):
+// 		// 	contentType = "text/html"
+// 		// case strings.HasSuffix(r.URL.Path, "css"):
+// 		// 	contentType = "text/css"
+// 		// case strings.HasSuffix(r.URL.Path, "png"):
+// 		// 	contentType = "image/png"
+// 		// default:
+// 		// 	contentType = "text/plain"
+// 		// }
+// 		// w.Header().Add("Content-Type", contentType)
+// 		io.Copy(w, f)
+// 	})
+// 	http.ListenAndServe(":8000", nil)
+// }
+
 func main() {
-	// log.Println("con co be be")
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	// 	w.Write([]byte("ha ha ha ha"))
-	// })
-	// http.Handle("/", &myHandler{greating: "bonjour"})
+	// templateString := `Lemonade Stand Supply`
+	// t, err := template.New("title").Parse(templateString)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	// err = t.Execute(os.Stdout, nil)
+	// if err != nil {
+	// 	log.Println(err)
+	// }
+	templates := populateTemplates()
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// http.ServeFile(w, r, "public"+r.URL.Path)
-		f, err := os.Open("public" + r.URL.Path)
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			log.Println(err)
+		requestedFile := r.URL.Path[1:]
+		t := templates.Lookup(requestedFile + ".html")
+		if t != nil {
+			if err := t.Execute(w, nil); err != nil {
+				log.Println(err)
+			}
+		} else {
+			w.WriteHeader(http.StatusNotFound)
 		}
-		defer f.Close()
-		// var contentType string
-		// switch {
-		// case strings.HasSuffix(r.URL.Path, "html"):
-		// 	contentType = "text/html"
-		// case strings.HasSuffix(r.URL.Path, "css"):
-		// 	contentType = "text/css"
-		// case strings.HasSuffix(r.URL.Path, "png"):
-		// 	contentType = "image/png"
-		// default:
-		// 	contentType = "text/plain"
-		// }
-		// w.Header().Add("Content-Type", contentType)
-		io.Copy(w, f)
 	})
+	http.Handle("/img/", http.FileServer(http.Dir("public")))
+	http.Handle("/css/", http.FileServer(http.Dir("public")))
 	http.ListenAndServe(":8000", nil)
+}
+
+func populateTemplates() *template.Template {
+	result := template.New("templates")
+	const basePath = "templates"
+	template.Must(result.ParseGlob(basePath + "/*.html"))
+	return result
 }
