117c2ff6e9cbdbe69fbb7060e696a9127cc0b887
hung.han <hung.han@setel.com>
2022-07-11 18:31:27 +0700

    go fund update

--- go_fundamental/hello-world/hello.go
+++ go_fundamental/hello-world/hello.go
@@ -1,14 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
 
 var (
 	name, course string
-	module, clip int
+	module       = "4"
+	clip         = 2
 )
 
 func main() {
 	fmt.Println("hello Pluralsight!")
 	fmt.Println("Name and course are set to", name, "and", course, ".")
 	fmt.Println("Module and clip are set to", module, "and", clip, ".")
+
+	iModule, err := strconv.Atoi(module)
+	if err == nil {
+		total := iModule + clip
+		log.Println("total of module and clip", total)
+	}
 }
