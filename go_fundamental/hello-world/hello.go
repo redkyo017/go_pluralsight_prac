76d0921a97ff46efbb82257069545c0c079cf12a
hung.han <hung.han@setel.com>
2022-07-11 18:44:19 +0700

    go fund update

--- go_fundamental/hello-world/hello.go
+++ go_fundamental/hello-world/hello.go
@@ -3,19 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
+	"reflect"
 	"strconv"
 )
 
-var (
-	name, course string
-	module       = "4"
-	clip         = 2
-)
-
 func main() {
+	name := "hung han"
+	course := "go fund"
+	module := "4"
+	clip := 2
+
 	fmt.Println("hello Pluralsight!")
 	fmt.Println("Name and course are set to", name, "and", course, ".")
 	fmt.Println("Module and clip are set to", module, "and", clip, ".")
+	fmt.Println("Clip is of type", reflect.TypeOf(clip))
+	fmt.Println("Module is of type", reflect.TypeOf(module))
+
+	fmt.Println("memory address of *course* variable is", &course)
+	var ptr *string = &course
+	fmt.Println("pointing course variable at address", ptr, "which notes this value", *ptr)
 
 	iModule, err := strconv.Atoi(module)
 	if err == nil {
